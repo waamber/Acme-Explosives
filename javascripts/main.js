9743944aa5fd8326ab8fa1d14b76a3298a68d85f
Waamber <amberastuart@aol.com>
2017-10-13 13:35:07 -0500

    returned json data

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
+const data = require('./data');
 
-console.log('hey');
+$(document).ready(() => {//document.ready doesn't run code until page is loaded.
+  data.initializer();
+});
+
+// $('#fireworks').click((e) => {
+//   console.log(e);
+//   data.categories();
+//   data.types();
+//   data.products();
+// });
